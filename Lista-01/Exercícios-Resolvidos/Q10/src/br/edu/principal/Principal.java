ee159346ccbc1bd9ea4cbd4d45d1ca2210d294cd
Murilo Silva <silva.murilo06@aluno.ifce.edu.br>
2025-08-30 08:46:26 -0300

    Add files via upload

---
+++ Lista-01/Exercícios-Resolvidos/Q10/src/br/edu/principal/Principal.java
@@ -0,0 +1,17 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o raio do círculo: ");
+		double raio = sc.nextDouble();
+		
+		double area = 3.1415 * raio * raio;
+		
+		System.out.println("A área do círculo é: " + area);
+	}
+
+}
